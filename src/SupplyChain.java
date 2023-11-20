e34e02534875045d41ddd1977b373f73b52ef504
Nhat Pham <99010618+nhatpham001@users.noreply.github.com>
2023-11-20 17:45:51 -0500

    Add files via upload

---
+++ src/SupplyChain.java
@@ -0,0 +1,11 @@
+package edu.uc.cs3003.medava;
+
+public class SupplyChain {
+    public static void main(String args[]) {
+        //System.out.println("Hello, <your name>! This is the WOPR speaking.");
+        HospitalRunner.run();
+    }
+}
+
+//checkpoint 5
+
